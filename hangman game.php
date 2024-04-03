2e29f0531879831082869a2c4fcee06552d62a6d
Mikele Mirko Vela <160608769+MikeleSail@users.noreply.github.com>
2024-04-03 09:55:43 +0200

    Add files via upload

---
+++ hangman game.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Hangman Game</title>
+</head>
+
+<body>
+    <link href="style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <div>
+        <h2>SCEGLI IL NUMERO DI GIOCATORI</h2>
+        <button id="ngiocatori">
+            <img id="playerimg" src="immagini_gioco\player.png">
+            <br>
+            <h3>1 GIOCATORE</h3>
+        </button>
+        <button id="ngiocatori">
+            <img id="playerimg" src="immagini_gioco\player.png"><img id="playerimg" src="immagini_gioco\player.png">
+            <br>
+            <h3>2 GIOCATORI</h3>
+        </button>
+        <button id="ngiocatori">
+            <img id="playerimg" src="immagini_gioco\player.png"><img id="playerimg" src="immagini_gioco\player.png"><img id="playerimg" src="immagini_gioco\player.png">
+            <br>
+            <h3>3 GIOCATORI</h3>
+        </button>
+        <button id="ngiocatori">
+            <img id="playerimg" src="immagini_gioco\player.png"><img id="playerimg" src="immagini_gioco\player.png"><img id="playerimg" src="immagini_gioco\player.png"><img id="playerimg" src="immagini_gioco\player.png"><br>
+            <br>
+            <h3>4 GIOCATORI</h3>
+        </button>
+    </div>
+    <?php
+
+    ?>
+</body>
+
+</html>

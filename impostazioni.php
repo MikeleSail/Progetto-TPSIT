22c70dcec40e2506540b4e1fd9c512887278b697
Mikele Mirko Vela <160608769+MikeleSail@users.noreply.github.com>
2024-04-09 14:11:11 +0200

    impostazioni.php

--- impostazioni.php
+++ impostazioni.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
-        <head>
-            <title>Hangman Game</title>
-        </head>
-    <!-- LINK CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="style.css" rel="stylesheet">
+
+<head>
+    <title>Hangman Game</title>
+</head>
+<!-- LINK CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link href="style.css" rel="stylesheet">
+
 <body>
     <!-- CANZONE -->
     <audio autoplay loop id="canzone">
@@ -13,37 +15,37 @@
     </audio>
 
     <?php
-// Inizializzazione del numero di round
-$rounds = 1;
+    // Inizializzazione del numero di round
+    $rounds = 1;
 
-// Verifica se è stato inviato un valore attraverso il parametro GET
-if(isset($_GET['rounds'])) {
-    $rounds = (int)$_GET['rounds'];
-    
-    // Limita il numero di round tra 1 e 10
-    $rounds = max(1, min(10, $rounds));
-}
+    // Verifica se è stato inviato un valore attraverso il parametro GET
+    if (isset($_GET['rounds']) == true) {
 
-// Verifica se è stato cliccato il pulsante "+" per incrementare i round
-if(isset($_GET['increment']) && $_GET['increment'] === '+') {
-    $rounds++;
-}
+        // Limita il numero di round tra 1 e 10
+        $rounds = max(1, min(10, $rounds));
+    }
 
-// Verifica se è stato cliccato il pulsante "-" per decrementare i round
-if(isset($_GET['decrement']) && $_GET['decrement'] === '-' && $rounds > 1) {
-    $rounds--;
-}
-?>
+    // Verifica se è stato cliccato il pulsante "+" per incrementare i round
+    if (isset($_GET['increment']) == true) {
+        $rounds++;
+    }
+
+    // Verifica se è stato cliccato il pulsante "-" per decrementare i round
+    if (isset($_GET['decrement']) == true && $rounds > 1) {
+        $rounds--;
+    }
+    ?>
 
     <!-- SCRITTA HANGMAN GAME - LOGO -->
-<img id="logo" src="immagini_gioco\hangman_game.png">
+    <img id="logo" src="immagini_gioco\hangman_game.png">
     <h1>NUMERO ROUND: <?php echo $rounds; ?></h1>
-    <form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <div class="bg-info clearfix" id="divround">
-    <input type="submit" name="decrement" class="btn btn-secondary float-start" value="-<?php echo $rounds > 1 ? ' ' : ''; ?>">
-    <br>
-    <h1 style="text-align: center;" id="nround" ><?php echo $rounds?></h1>
-    <input type="submit" name="increment" value="+ " class="btn btn-secondary float-end">
-    </div>
+    <form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <div class="bg-info clearfix" id="divround">
+            <input type="submit" name="decrement" class="btn btn-secondary float-start" value="-<?php echo $rounds > 1 ? ' ' : ''; ?>">
+            <br>
+            <h1 style="text-align: center;" id="nround"><?php echo $rounds ?></h1>
+            <input type="submit" name="increment" value="+ " class="btn btn-secondary float-end">
+        </div>
 </body>
+
 </html>
